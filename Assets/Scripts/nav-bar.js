f9fe4c6943ee8cde95187794b4876fc20f4409e1
Cristian Andriel Silva <contato.cristianandriel@gmail.com>
2024-11-23 12:25:54 -0300

    Update 0.2

---
+++ Assets/Scripts/nav-bar.js
@@ -0,0 +1,10 @@
+// Função para abrir o menu
+function openMenu() {
+    document.getElementById("fullscreenMenu").style.width = "100%";
+  }
+  
+  // Função para fechar o menu
+  function closeMenu() {
+    document.getElementById("fullscreenMenu").style.width = "0";
+  }
+  
