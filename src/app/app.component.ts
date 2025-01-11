d863d58dae9088441e90fa353b1eba1fbeeb6b73
CristianAndriel <cristianandrielsilva@gmail.com>
2025-01-11 16:25:20 -0300

    Adicionando as novas sessões

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [HomeComponent, AboutComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
